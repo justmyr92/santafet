ecdc2c64824b0285fa905d56adaf49bced4f7f38
MarkRanielPerez <20-06887@g.batstate-u.edu.ph>
2023-11-23 07:35:32 +0800

    deploy

--- backend/db/sfm_db.js
+++ backend/db/sfm_db.js
@@ -3,7 +3,7 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
     user: "postgres",
     password: "dyasmir",
-    host: "localhost",
+    host: "54.179.35.184",
     port: 5432,
     database: "sfm",
 });
